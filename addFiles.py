b6dbc35d2a9299eb6ade2544513b73d3ffae0773
DogeMuchHapp <83430363+DogeMuchHapp@users.noreply.github.com>
2026-07-18 23:28:20 -0400

    dd

--- addFiles.py
+++ addFiles.py
@@ -2,7 +2,7 @@ import os
 import json
 
 folder = "My Computer"
-files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f)) and f != "manifest.json"]
 
 with open(os.path.join(folder, "manifest.json"), "w") as f:
     json.dump(files, f, indent=2)
